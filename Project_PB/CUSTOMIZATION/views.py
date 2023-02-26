c2f7b7ba1e5ada82e1cc8bee02345327b24ebb1a
Zarna Pathak <pathakzarna0128@gmail.com>
2023-02-26 13:30:41 +0530

    Zarna/Customization/Linked Hoops Pages
    Generated Request/path and Display Function for Birthday , Wedding , Best Person & Name Hoop

--- Project_PB/CUSTOMIZATION/views.py
+++ Project_PB/CUSTOMIZATION/views.py
@@ -6,3 +6,39 @@ def custom(request):
 
 def hoops(request):
     return render(request,'hoop.html')
+
+def bestperson(request):
+    return render(request,'bestperson.html')
+
+def bestperson1(request):
+    return render(request,'1_bestperson_hoop.html')
+
+def bestperson2(request):
+    return render(request,'2_bestperson_hoop.html')
+
+def namehoop(request):
+    return render(request,'name_hoop.html')
+
+def namehoop1(request):
+    return render(request,'1_name_hoop.html')
+
+def namehoop2(request):
+    return render(request,'2_name_hoop.html')
+
+def weddinghoop(request):
+    return render(request,'weddinghoop.html')
+
+def weddinghoop1(request):
+    return render(request,'1_wed_hoop.html')
+
+def weddinghoop2(request):
+    return render(request,'2_wed_hoop.html')
+
+def birthdayhoop(request):
+    return render(request,'birthdayhoop.html')
+
+def birthdayhoop1(request):
+    return render(request,'1_birthday_hoop.html')
+
+def birthdayhoop2(request):
+    return render(request,'2_birthday_hoop.html')
--- Project_PB/Project_PB/settings.py
+++ Project_PB/Project_PB/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME':'PROMISING_BEAUTY',
         'USER':'postgres',
-        'PASSWORD':'12345',
+        'PASSWORD':'PG12345',
         'HOST':'localhost',
         'PORT':'5432'
         }
