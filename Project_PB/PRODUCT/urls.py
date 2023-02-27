178dfbaf7674cce14cd372e61f64023bcc99ddd0
Zarna Pathak <pathakzarna0128@gmail.com>
2023-02-27 23:31:57 +0530

    Zarna/Product/remove_wish & remove_bag
    Remove product from Bag and Wishlist function implemented

--- Project_PB/PRODUCT/urls.py
+++ Project_PB/PRODUCT/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('add-to-wishlist/<int:id>',views.addToWishlist,name='add-to-wishlist'),
     path('show_wishlist',views.show_wish,name='show_wishlist'),
     path('add-to-bag/<int:id>',views.addTobag,name='add-to-bag'),
-    path('show_bag',views.show_bag,name='show_bag')
+    path('show_bag',views.show_bag,name='show_bag'),
+    path('remove_wish/<int:id>',views.remove_wish,name='remove_wish'),
+    path('remove_bag/<int:id>',views.remove_bag,name='remove_bag')
 ]
